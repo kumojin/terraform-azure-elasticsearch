b5f3b7085d449c0ae351935d131e1e136fc6ead0
Marc de Klerk <deklerkmc@gmail.com>
2021-01-28 17:54:49 +0200

    fixes

--- templates/user_data.kibana.sh
+++ templates/user_data.kibana.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-echo "xpack.security.enabled: ${security_enabled}" | sudo tee -a /etc/kibana/kibana.yml
-echo "xpack.monitoring.enabled: ${monitoring_enabled}" | sudo tee -a /etc/kibana/kibana.yml
-echo "xpack.reporting.enabled: false" | sudo tee -a /etc/kibana/kibana.yml
-
-echo "elasticsearch.hosts:"
+echo "elasticsearch.hosts:" | sudo tee -a /etc/kibana/kibana.yml
 for i in $(seq -f "%06g" 0 ${data_node_count})
 do
     echo "- http://default-data$i:9200" | sudo tee -a /etc/kibana/kibana.yml
